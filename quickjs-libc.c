882c0af850f5d037b9e55fc5a5199683922a700a
Vasyl Havryliak <gavrilyak@gmail.com>
2024-03-01 14:34:29 +0200

    LARGE_FILE patch is back

--- quickjs-libc.c
+++ quickjs-libc.c
@@ -22,6 +22,9 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
+#define _LARGEFILE64_SOURCE 1
+#define _FILE_OFFSET_BITS 64
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdarg.h>
